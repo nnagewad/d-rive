6d3f29e1fdfbedd6f469a44e9a4ee4a110fc5f1a
nnagewad <nikin.nagewadia@icloud.com>
2025-12-16 20:01:44 +0000

    Start of project

---
+++ Dérive/DériveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DériveApp.swift
+//  Dérive
+//
+//  Created by Nikin Nagewadia on 2025-12-16.
+//
+
+import SwiftUI
+
+@main
+struct DériveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
